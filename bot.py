d5413d96675b1c1a634749165056d3925528a28e
tuckismad <tuckismad@gmail.com>
2015-02-24 00:54:32 +1100

    Added some safeties & Documentation

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 Tuck made this thing
 '''
 
-
+# Imports
 import socket
 import requests
 import os
@@ -11,10 +11,12 @@ from flask import Flask, render_template
 import threading
 import re
 
+# Global Variables
 previousStreamsLink = "http://www.watchpeoplecode.com/past_streams"
 apiUrl = "http://www.watchpeoplecode/json"
 
 
+# Website Part
 class Website:
     def __init__(self, bot):
         self.logs = bot.logs
@@ -30,29 +32,38 @@ class Website:
 
 
 class Bot:
-    logs = []
+    logs = []  # Logs to pass to the website
 
     def __init__(self):
-        self.HOST = "irc.freenode.net"
-        self.PORT = 6667
-        self.username = os.environ.get('BOT_USERNAME', 'TuckBot_Local')
+        self.HOST = "irc.freenode.net"  # IRC Server
+        self.PORT = 6667  # IRC Server Port
+        self.username = os.environ.get('BOT_USERNAME', 'TuckBot_Local')  # Bot Username
         self.master = "tuckismad"
-        self.channel = os.environ.get('IRC_CHANNEL', '#WpcBotTesting')
-        self.password = os.environ.get('IRC_PASSWORD', '')
-        self.lastTime = time()
-        self.newCacheLiveStreams = self.JSONtoSet(self.GetJSON())
-        self.cacheLiveStreams = self.newCacheLiveStreams
-        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.irc.connect((self.HOST, self.PORT))
+        self.channel = os.environ.get('IRC_CHANNEL', '#WpcBotTesting')  # IRC Channel
+        self.password = os.environ.get('IRC_PASSWORD', '')  # Bot Password
+        self.lastLivestreamCheck = time()  # Livestream Check Timer
+        self.newCacheLiveStreams = self.JSONtoSet(self.GetJSON(), 'live')  # Cache of new liveStreams
+        self.cacheLiveStreams = self.newCacheLiveStreams  # Cache of old liveStreams
+        self.shouldCheckForLivestreams = True
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creating a socket for IRC
+        self.irc.connect((self.HOST, self.PORT))  # Use the socket to connect to IRC
 
-        self.irc.send(bytes("NICK " + self.username + "\r\n", "UTF-8"))
-        self.irc.send(bytes("USER " + self.username + " " + self.username + " " + self.username + " :PythonBot\r\n", "UTF-8"))
-        self.irc.send(bytes("PRIVMSG nickserv :identify " + self.password + "\r\n", "UTF-8"))
-        self.irc.send(bytes("JOIN " + self.channel + "\r\n", "UTF-8"))
+        self.irc.send(bytes("NICK " + self.username + "\r\n", "UTF-8"))  # Sends nick to IRC
+        self.irc.send(bytes("USER " + self.username + " " + self.username + " " + self.username + " :PythonBot\r\n", "UTF-8"))  # Sends user data to IRC
+        self.irc.send(bytes("PRIVMSG nickserv :identify " + self.password + "\r\n", "UTF-8"))  # Sends password to IRC
+        self.irc.send(bytes("JOIN " + self.channel + "\r\n", "UTF-8"))  # Join channel
 
+    '''
+    Send(String target, String message)
+    Sends 'message' to 'target'
+    '''
     def Send(self, name, message):
         self.irc.send(bytes("PRIVMSG " + name + " :" + message + "\r\n", "UTF-8"))
 
+    '''
+    Check(data, cmd)
+    Checks if the 'cmd' is in 'data'
+    '''
     def Check(self, data, cmd):
         data = data.lower()
         cmd = cmd.lower()
@@ -61,31 +72,39 @@ class Bot:
         else:
             return False
 
+    '''
+    GetJSON()
+    Returns a JsonObject from watchpeoplecode.com/json
+    '''
     def GetJSON(self, ):
         return requests.get('http://www.watchpeoplecode.com/json').json()
 
-    def JSONtoSet(self, jsonObject):
+    '''
+    JSONtoSet(jsonObject, row)
+    Returns a list of rows from a jsonObject
+    '''
+    def JSONtoSet(self, jsonObject, streamType):
         result = []
-        for row in jsonObject['live']:
+        for row in jsonObject[streamType]:
             result.append(row)
         return result
 
-    def SendLiveStreams(self):
+    '''
+    SendStreamList(streamType)
+    Sends a list of streams of type 'streamType' to IRC
+    '''
+    def SendStreamList(self, streamType):
         response = self.GetJSON()
-        if response['live'] == []:
-            self.Send(self.channel, "No current streams available :(")
+        if response[streamType] == []:
+            self.Send(self.channel, "There are no " + streamType + " streams :(")
         else:
-            for obj in response['live']:
+            for obj in response[streamType]:
                 self.Send(self.channel, ('Name: ' + obj['title'] + ' URL: ' + obj['url']))
 
-    def SendUpcomingStreams(self):
-        response = self.GetJSON()
-        if response['upcoming'] == []:
-            self.Send(self.channel, "There are no upcoming streams :(")
-        else:
-            for obj in response['upcoming']:
-                self.Send(self.channel, ('Name: ' + obj['title'] + ' URL: ' + obj['url']))
-
+    '''
+    ParseData(data)
+    Returns a list of tuples of parsed data
+    '''
     def ParseData(self, data):
         result = []
         sender_pattern = r'^:(.*)!'
@@ -96,43 +115,58 @@ class Bot:
             result.append({'sender': sender_match.group(1), 'message': message_match.group(1)})
         return result
 
+    '''
+    Run()
+    Main run function
+    '''
     def Run(self):
         while True:
-            try:
+            try:  # Try to decode the incoming data from IRC
                 data = self.irc.recv(4096).decode("UTF-8")
             except:
                 print("Decoding Failed")
-            if(data.find("PING") != -1):
+            if(data.find("PING") != -1):  # replies to pings from IRC
                 self.irc.send(bytes("PONG " + data.split()[1] + "\r\n", "UTF-8"))
 
-            if(self.Check(data, self.username)):
+            if(self.Check(data, self.username)):  # Check if Bot is being talked to
                 if(self.Check(data, "previous") and self.Check(data, "streams")):
                     self.Send(self.channel, previousStreamsLink)
-                if(self.Check(data, "what") and self.Check(data, "commands")):  # make dict of commands and output
+                if(self.Check(data, "what") and self.Check(data, "commands")):
                     self.Send(self.channel, "That is for me to know and you to find out when Tuck programs me to tell you ;)")
                 if(self.Check(data, "current") and self.Check(data, "streams")):
-                    self.SendLiveStreams()
+                    self.SendStreamList('live')
                 if(self.Check(data, "upcoming") and self.Check(data, "streams")):
-                    self.SendUpcomingStreams()
+                    self.SendStreamList('upcoming')
                 if(self.Check(data, "answer") and self.Check(data, "to") and self.Check(data, "life")):
                     self.Send(self.channel, "According to my memory bank, the answer is 42.")
                 if(self.Check(data, "eye") and self.Check(data, "on") and self.Check(data, "hcwool")):
                     self.Send(self.channel, "I'll keep an eye on him for you")
                 if(self.Check(data, "is") and self.Check(data, "hcwool") and self.Check(data, "takeover")):
                     self.Send(self.channel, "Yes, but his attempts are futile")
+                if(self.Check(data, "livestreamchecker") and self.Check(data, "on")):
+                    self.shouldCheckForLivestreams = True
+                    self.Send(self.channel, "LivestreamChecker set to: True")
+                if(self.Check(data, "livestreamchecker") and self.Check(data, "off")):
+                    self.shouldCheckForLivestreams = False
+                    self.Send(self.channel, "LivestreamChecker set to: False")
+                if(self.Check(data, "mute")):
+                    self.channel = ""
+                if(self.Check(data, "unmute")):
+                    self.channel = os.environ.get('IRC_CHANNEL', '#WpcBotTesting')
 
-            #if time() >= self.lastTime + 10:
-            #    self.lastTime = time()
-            #    newCacheLiveStreams = self.JSONtoSet(self.GetJSON())
-            #    for newStream in self.newCacheLiveStreams:
-            #        for oldStream in self.cacheLiveStreams:
-            #            if newStream == oldStream:
-            #                break
-            #        else:
-            #            self.Send(self.channel, '"' + newStream['title'] + '" just went live!, check it out here: ' + newStream['url'])
-            #    self.cacheLiveStreams = newCacheLiveStreams
+            if time() >= self.lastLivestreamCheck + 10:  # Checks for new livestreams every 10 seconds
+                self.lastLivestreamCheck = time()
+                if self.shouldCheckForLivestreams:
+                    self.newCacheLiveStreams = self.JSONtoSet(self.GetJSON(), 'live')
+                    for newStream in self.newCacheLiveStreams:
+                        for oldStream in self.cacheLiveStreams:
+                            if newStream == oldStream:
+                                break
+                        else:
+                            self.Send(self.channel, '"' + newStream['title'] + '" just went live!, check it out here: ' + newStream['url'])
+                    self.cacheLiveStreams = self.newCacheLiveStreams
 
-            parsedData = self.ParseData(data)
+            parsedData = self.ParseData(data)  # Used for logging to the flask website
             if parsedData:
                 for msg in parsedData:
                     self.logs.append(msg['sender'] + ": " + msg['message'])
@@ -145,3 +179,11 @@ if __name__ == '__main__':
     website = Website(bot)
     p2 = threading.Thread(target=website.Run)
     p2.start()
+
+# break messages with string delimiter for logging
+# GCD
+# Call Admins
+# Uptime
+# Timer converter
+# Regex
+# timezones
